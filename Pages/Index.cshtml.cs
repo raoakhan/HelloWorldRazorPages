790017e7413c81a7ac469acb293ab7766fa984a3
akmal_shahbaz@yahoo.com <akmal_shahbaz@yahoo.com>
2021-07-22 13:04:32 +0500

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using HelloWorldRazorPages.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorldRazorPages.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public AppMessage Heading { get; set; }
+        public String SubHeading { get; set; }
+        
+        public void OnGet()
+        {
+            this.Heading = new AppMessage();
+            this.Heading.Message = "Hello World!";
+        }
+
+        public void onPost()
+        {
+            this.SubHeading = "Message changed.";
+        }
+
+
+
+
+    }
+}
